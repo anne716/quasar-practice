ed6c0b7e9e2540cec295b359ef7f3432d9df913f
anna <anniegabriento51@gmail.com>
2025-01-06 14:05:22 +0800

    javascript events

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,6 +1,22 @@
 <template>
   <q-page padding>
-    <input v-model="message">
+    <!-- Using keyboard events without shortcut -->
+    <!-- <input v-model="message" @keyup="handleKeyup"> -->
+
+    <!-- Shortcut for keyboard events: @keyup.<key-name> -->
+    <!-- Demo for mouse events @mouse -->
+    <!-- <input
+      v-model="message"
+      @keyup.esc="clearMessage"
+      @keyup.enter="alertMessage"
+      @mouseleave="alertMessage"
+    > -->
+
+    <input
+      v-model="message"
+      @keyup.esc="clearMessage"
+      @keyup.enter="alertMessage"
+    >
     <button @click="clearMessage">Clear</button>
     <h5>{{ message }}</h5>
   </q-page>
@@ -16,7 +32,20 @@
     methods: {
       clearMessage() {
         this.message = ""
-      }
+      },
+      alertMessage() {
+        alert(this.message)
+      },
+      // handleKeyup(e) {
+      //   console.log(e) // To check the keyboard attributes (e.g. keyCode)
+
+      //   if (e.keyCode == 27) {
+      //     this.clearMessage()
+      //   }
+      //   else if (e.keyCode == 13) {
+      //     this.alertMessage()
+      //   }
+      // }
     }
   }
 </script>
