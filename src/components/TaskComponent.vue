b241f324546b7b70a7f598a3dedc2277d27db0c5
anna <anniegabriento51@gmail.com>
2025-01-06 17:51:57 +0800

    props

---
+++ src/components/TaskComponent.vue
@@ -0,0 +1,17 @@
+  <template>
+    <li>
+      <div>{{ task.name }}</div>
+      <small>{{ task.dueDate }} @ {{ task.dueTime }}</small>
+      <button @click="deleteTask(index)">Delete</button>
+    </li>
+  </template>
+
+  <script>
+  export default {
+    props: ['task', 'index', 'deleteTask']
+  }
+  </script>
+
+  <style scoped>
+
+  </style>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,44 +1,52 @@
 <template>
   <q-page padding>
     <ul>
-      <li v-for="(task, index) in tasks" :key="index">
-        <div>{{ task.name }}</div>
-        <small>{{ task.dueDate }} @ {{ task.dueTime }}</small>
-        <button @click="deleteTask(index)">Delete</button>
-      </li>
+      <task
+        v-for="(task, index) in tasks"
+        :key="index"
+        :task="task"
+        :index="index"
+        :delete-task="deleteTask"
+      >
+      </task>
     </ul>
   </q-page>
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
-        tasks: [
-          {
-            name: 'Go to shop',
-            dueDate: '2019/05/12',
-            dueTime: '18:30'
-          },
-          {
-            name: 'Get bananas',
-            dueDate: '2020/01/12',
-            dueTime: '13:30'
-          },
-          {
-            name: 'Get apples',
-            dueDate: '2018/07/21',
-            dueTime: '12:30'
-          }
-        ]
-      }
-    },
-    methods: {
-      deleteTask(index) {
-        this.tasks.splice(index, 1)
-      }
+import TaskComponent from 'components/TaskComponent.vue';
+
+export default {
+  data() {
+    return {
+      tasks: [
+        {
+          name: 'Go to shop',
+          dueDate: '2019/05/12',
+          dueTime: '18:30'
+        },
+        {
+          name: 'Get bananas',
+          dueDate: '2020/01/12',
+          dueTime: '13:30'
+        },
+        {
+          name: 'Get apples',
+          dueDate: '2018/07/21',
+          dueTime: '12:30'
+        }
+      ]
     }
+  },
+  methods: {
+    deleteTask(index) {
+      this.tasks.splice(index, 1)
+    }
+  },
+  components: {
+    'task' : TaskComponent
   }
+}
 </script>
 
 <style>
